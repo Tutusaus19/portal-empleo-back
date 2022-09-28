c9adfa6bda3e502a0506b0e6d21982f70e1e0c1d
Charlie Provisional <charlieprovisional@MacBook-Air-de-Paula.local>
2022-09-28 18:48:25 +0200

    chore/db added

--- environments/local.js
+++ environments/local.js
@@ -1,4 +1,4 @@
 module.exports = {
     PORT: 3000,
-    DB_URL: "postgres://admin:1234@localhost:5432/portal_empleo"
+    DB_URL: "postgresql://postgres:zCcgXoWATfTFYwE1MWKO@containers-us-west-50.railway.app:7039/railway"
 };
